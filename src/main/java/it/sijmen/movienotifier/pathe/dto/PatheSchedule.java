45537dbcd32031dcfd3327d26984d5a0e8c6577a
jpelgrom <jpelgrom@users.noreply.github.com>
2017-04-09 21:51:21 +0200

    Add missing JSON names to PatheSchedule (showing object)
     - Added all JSON names which were not yet present in the object, excluding showId, specialCode and deeplink

--- src/main/java/it/sijmen/movienotifier/pathe/dto/PatheSchedule.java
+++ src/main/java/it/sijmen/movienotifier/pathe/dto/PatheSchedule.java
@@ -23,6 +23,22 @@ public class PatheSchedule implements Serializable {
     private int isImax;
     @JsonProperty("ov")
     private int isOriginalVersion;
+    @JsonProperty("vip")
+    private int isVIPAvailable;
+    @JsonProperty("hfr")
+    private int isHfr;
+    @JsonProperty("isAtmos")
+    private int isAtmos;
+    @JsonProperty("is4k")
+    private int is4k;
+    @JsonProperty("isLaser")
+    private int isLaser;
+    @JsonProperty("isPremium")
+    private int isPremium;
+    @JsonProperty("hallIsLarge")
+    private int isLargeHall;
+    @JsonProperty("hallIsRooftop")
+    private int isRooftopHall;
     @JsonProperty
     private long movieId;
     @JsonProperty("id")
@@ -136,5 +152,69 @@ public class PatheSchedule implements Serializable {
 
     public void setSpecialId(long specialId) {
         this.specialId = specialId;
+    }
+
+    public int getIsVIPAvailable() {
+        return isVIPAvailable;
+    }
+
+    public void setIsVIPAvailable(int isVIPAvailable) {
+        this.isVIPAvailable = isVIPAvailable;
+    }
+
+    public int getIsHfr() {
+        return isHfr;
+    }
+
+    public void setIsHfr(int isHfr) {
+        this.isHfr = isHfr;
+    }
+
+    public int getIsAtmos() {
+        return isAtmos;
+    }
+
+    public void setIsAtmos(int isAtmos) {
+        this.isAtmos = isAtmos;
+    }
+
+    public int getIs4k() {
+        return is4k;
+    }
+
+    public void setIs4k(int is4k) {
+        this.is4k = is4k;
+    }
+
+    public int getIsLaser() {
+        return isLaser;
+    }
+
+    public void setIsLaser(int isLaser) {
+        this.isLaser = isLaser;
+    }
+
+    public int getIsPremium() {
+        return isPremium;
+    }
+
+    public void setIsPremium(int isPremium) {
+        this.isPremium = isPremium;
+    }
+
+    public int getIsLargeHall() {
+        return isLargeHall;
+    }
+
+    public void setIsLargeHall(int isLargeHall) {
+        this.isLargeHall = isLargeHall;
+    }
+
+    public int getIsRooftopHall() {
+        return isRooftopHall;
+    }
+
+    public void setIsRooftopHall(int isRooftopHall) {
+        this.isRooftopHall = isRooftopHall;
     }
 }
