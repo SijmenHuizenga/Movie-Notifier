cb3ebe57a409be30142c9d2e3645de30b4ee8a53
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-04-13 18:06:02 +0200

    fix bugs

--- src/main/java/it/sijmen/movienotifier/inj/ConfigurationModule.java
+++ src/main/java/it/sijmen/movienotifier/inj/ConfigurationModule.java
@@ -12,15 +12,20 @@ import java.util.Properties;
  */
 public class ConfigurationModule extends AbstractModule {
 
-    private final String configFileName = "/movie-notifier.properties";
+    private final String configFileName = "movie-notifier.properties";
 
     @Override
     protected void configure() {
 
         try {
-            File f = new File(getClass().getResource(configFileName).toURI());
+            File f;
+            if(runningFromJar())
+                f = new File(configFileName);
+            else
+                f = new File(getClass().getResource("/"+configFileName).toURI());
+
             if(!f.exists())
-                throw new IllegalArgumentException("Configuration file could not be found. Check if the file "+configFileName+" is available.");
+                throw new IllegalArgumentException("Configuration file could not be found. Check if the file "+f.getAbsolutePath()+" is available.");
 
             Properties properties = new Properties();
             properties.load(new FileReader(f));
@@ -28,6 +33,18 @@ public class ConfigurationModule extends AbstractModule {
         } catch (Exception e) {
             throw new IllegalArgumentException("Configuration file could not be loaded. Check if the file "+configFileName+" is available and valid. Error message: " + e.getMessage(), e);
         }
+    }
+
+    public static String getJarName() {
+        return new File(ConfigurationModule.class.getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .getPath())
+                .getName();
+    }
+
+    public static boolean runningFromJar() {
+        return getJarName().contains(".jar");
     }
 
 }
